15ebfaebf0130a8ef93b117b6384229ee44b0392
vBlanyer <vblanyer@gmail.com>
2025-11-03 15:51:24 -0400

    [FEAT] parseCommand add

--- src/Engine/Parser.hs
+++ src/Engine/Parser.hs
@@ -1,8 +1,18 @@
 module Engine.Parser (parseCommand) where
-
+import Data.Char (toLower)
 import Engine.Types
 
 -- Parsea la entrada del usuario (String) a un (Maybe Command)
 parseCommand :: String -> Maybe Command
-parseCommand input =
-  undefined
+parseCommand input = case words input of
+  [x] | x == "mirar" -> Just Mirar
+      | x == "inventario" -> Just Inventario 
+      | x == "salir" -> Just Salir
+  ("tomar": name) | not (null name) -> Just (Tomar (Item (unwords name) "Sin descripción"))
+  ("ir": direction) | not (null direction) -> 
+    case map toLower (unwords direction) of
+      "norte" -> Just (Ir Norte)
+      "este" -> Just (Ir Este)
+      "sur" -> Just (Ir Sur)
+      "oeste" -> Just (Ir Oeste)
+  _ -> Nothing

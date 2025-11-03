a1a3a7f31bc68b6eebd831ab7774e9e0cdd8d4af
vBlanyer <vblanyer@gmail.com>
2025-11-03 15:50:23 -0400

    [FIX] Changes in definitions and renaming of attributes

--- src/Engine/Types.hs
+++ src/Engine/Types.hs
@@ -2,15 +2,18 @@ module Engine.Types where
 import qualified Data.Map as Map
 
 -- Direcciones posibles
-data Direction = Norte | ESTE | OESTE | SUR -- -> Devuelve Nothing si usa con Maybe
+data Direction = Norte | Este | Oeste | Sur -- -> Devuelve Nothing si usa con Maybe
 
 -- Un objeto en el juego
-newtype Item = Item {itemDescrition :: String} -- Por ahora solo tenemos descripcion para un objeto, nada mas
+data Item = Item {
+      name :: String,
+      itemDescription :: String
+}
 
 -- Una sala en el juego
 data Room = Room {
       id :: Integer, -- Seria bueno supongo, para buscarlo en tal caso
-      description :: String,
+      roomDescription :: String,
       roomExits :: Map.Map Direction Room, -- Asi tenemos par clave valor, para poder tener las direcciones con su room asociado
       items :: [Item]
 } -- Hay que buscar la forma de que sea por referencia, para que la misma Room que se encuentra en el Map sea el que se encuentra en world
@@ -23,8 +26,13 @@ data GameState = GameState {
 }
 
 -- Comandos que el jugador puede ejecutar
-data Command = Direccion | Tomar String  -- Seguramente falten mas
+data Command -- Hay que tomar en cuenta unas variantes ligeras que salen en el enunciado
+      = Ir Direction
+      | Mirar
+      | Tomar Item
+      | Inventario
+      | Salir
 
--- -- Placeholders para loadWorldData
--- type RoomContainer = ()
--- type ItemContainer = ()
+-- Placeholders para loadWorldData
+type RoomName = ()
+type ItemName = ()

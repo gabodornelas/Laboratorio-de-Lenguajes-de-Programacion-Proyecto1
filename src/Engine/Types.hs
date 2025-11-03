11ee6a2c271df248c9420cbcd7df3ba89b611613
vBlanyer <vblanyer@gmail.com>
2025-11-03 12:47:42 -0400

    [FEAT] Defined address, game state, objects, command, objects and room

--- src/Engine/Types.hs
+++ src/Engine/Types.hs
@@ -1,20 +1,30 @@
 module Engine.Types where
+import qualified Data.Map as Map
 
 -- Direcciones posibles
-data Direction = Dummy1
+data Direction = Norte | ESTE | OESTE | SUR -- -> Devuelve Nothing si usa con Maybe
 
 -- Un objeto en el juego
-data Item = Dummy2
+newtype Item = Item {itemDescrition :: String} -- Por ahora solo tenemos descripcion para un objeto, nada mas
 
 -- Una sala en el juego
-data Room = Dummy3
+data Room = Room {
+      id :: Integer, -- Seria bueno supongo, para buscarlo en tal caso
+      description :: String,
+      roomExits :: Map.Map Direction Room, -- Asi tenemos par clave valor, para poder tener las direcciones con su room asociado
+      items :: [Item]
+} -- Hay que buscar la forma de que sea por referencia, para que la misma Room que se encuentra en el Map sea el que se encuentra en world
 
 -- El estado completo del juego
-data GameState = Dummy4
+data GameState = GameState {
+      room :: Room,
+      inventory :: [Item],
+      world :: [Room] 
+}
 
 -- Comandos que el jugador puede ejecutar
-data Command = Dummy5
+data Command = Direccion | Tomar String  -- Seguramente falten mas
 
--- Placeholders para loadWorldData
-type RoomContainer = ()
-type ItemContainer = ()
+-- -- Placeholders para loadWorldData
+-- type RoomContainer = ()
+-- type ItemContainer = ()

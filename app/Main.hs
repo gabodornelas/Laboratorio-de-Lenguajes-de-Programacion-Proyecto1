bc2de4e4ccbe582ce36fe523a3d182f31590e507
vBlanyer <vblanyer@gmail.com>
2025-11-04 10:25:40 -0400

    [FEAT]foundation for developing the Core

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 -- app/Main.hs
 module Main where
-
+import qualified Data.Map as Map
 import Engine.Types
 import Engine.Parser
 import Engine.Core
@@ -13,7 +13,33 @@ main = do
     -- Manejar el resultado de la carga
     -- Crear el estado inicial
     -- Iniciar el bucle del juego
-    undefined
+
+    -- Mundo de ejemplo. Porque hara falta Persistence. Por ahora toca directo en codigo
+    -- Item inicial correcto
+    let item1 = Item {
+        itemName = "item1",
+        itemDescription = "Item de prueba"
+    }
+
+    -- Inicial room necesita de algo, tengo problmeas aqui con
+    let initialRoom = Room {
+        roomId = 1,
+        roomDescription = "Primer cuarto",
+        roomItems = [item1],
+        roomExits = Just Map.empty
+    }
+
+    -- Creacion de mapa, por dedcirlo de una forma, el grafo
+    let mapa = Map.fromList [("norte", initialRoom)]
+
+    -- Estado inicial
+    let state = GameState {
+        room = initialRoom,
+        inventory = [item1],
+        world = [initialRoom]
+    }
+
+    gameLoop state
 
 
 -- El bucle principal del juego
@@ -24,4 +50,15 @@ gameLoop state = do
     -- Procesar el comando
     -- Mostrar resultado
     -- Continuar el bucle con el nuevo estado
-    undefined
+
+
+    putStrLn "> "
+    input <- getLine
+    putStrLn ("Comando introducido: " ++ input) 
+    case parseCommand input of -- solo me funciono con minusculas (Correrig luego para todo tipo en Parser)
+        Just dir -> do 
+            putStrLn ("Direccion reconocida")
+            let (mensaje, estado) = processCommand dir state
+            putStrLn mensaje
+        Nothing  -> putStrLn "No se reconoció la dirección"
+
--- src/Engine/Core.hs
+++ src/Engine/Core.hs
@@ -4,7 +4,8 @@ import Engine.Types
 
 -- La función PURA que actualiza el estado del juego
 processCommand :: Command -> GameState -> (String, GameState)
-processCommand command state =
+processCommand command state = case command of
+  Mirar -> ("El persona Miro", state)
   -- Pista: usar pattern matching para manejar cada comando (Ir, Tomar, Mirar, etc.)
   -- Devuelve siempre una tupla de (Mensaje para el usuario, NuevoEstado)
-  undefined
+  -- Blanyer: Considero que hay que hacer primer un case para determianr que tipo de comando se esta pasando, y realizar la accion a consecuencia
